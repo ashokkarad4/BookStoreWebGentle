c2a3f97a3f2d7d0e8bc7977d629ae5ae7827e84e
ashok.karad <ashokkarad4@gmail.com>
2022-11-10 17:39:23 +0530

    Added mvc to application

--- BookStoreWebGentle/BookStoreWebGentle/Startup.cs
+++ BookStoreWebGentle/BookStoreWebGentle/Startup.cs
@@ -16,6 +16,7 @@ namespace BookStoreWebGentle
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,7 +34,7 @@ namespace BookStoreWebGentle
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello Ashok!");
+                    await context.Response.WriteAsync("Hello Ashok!" );
                 });
             });
 
